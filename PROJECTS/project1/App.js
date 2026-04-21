4195f6b60bb254d089cdc044ac1f71b1f208e8d1
shrinivas-sn <shrinusn2001@gmail.com>
2026-04-21 16:04:24 +0530

    Updated goals app with flexbox

--- PROJECTS/project1/App.js
+++ PROJECTS/project1/App.js
@@ -1,46 +1,45 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text1}>Hello world</Text>
-      <Text style={styles.text2}>Second Line</Text>
-      <Text style={styles.text3}>Third Line</Text>
-      <Button title="Click Me" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
+  inputContainer: {
+    color: "blue",
+    backgroundColor: "#0000",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column",
-  },
-
-  text1: {
-    width: "150",
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "red",
+    flexDirection: "row",
+    margin: 20,
     padding: 20,
+    borderBottomWidth: 0.5,
   },
 
-  text2: {
-    width: "150",
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "yellow",
-    padding: 20,
-  },
-
-  text3: {
-    width: "150",
-    margin: 16,
+  textInput: {
+    margin: 10,
+    borderRadius: 10,
+    width: "90%",
     borderWidth: 2,
     borderColor: "blue",
-    padding: 20,
+  },
+
+  list: {
+    width: "150",
+    margin: 10,
+  },
+
+  goalsContainer: {
+    margin: 10,
   },
 });
