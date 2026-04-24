efa370879ce020e736750b67e4a09c2b7d9271b6
shrinivas-sn <shrinusn2001@gmail.com>
2026-04-24 15:41:38 +0530

    Updated app logic with alert screen popup

--- PROJECTS/3-number-game/screens/StartGameScreen.js
+++ PROJECTS/3-number-game/screens/StartGameScreen.js
@@ -1,60 +1,89 @@
-import { TextInput, View } from "react-native";
-import PrimaryButtons from "../components/PrimaryButtons";
-import { StyleSheet } from 'react-native';
-export default function StartGameScreen() {
-    return (
+import { useState } from 'react';
+import { TextInput, View, StyleSheet, Alert } from 'react-native';
 
-        <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" />
-            <View style={styles.buttonsContainer}>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButtons>Reset</PrimaryButtons>
-                </View>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButtons>Confirm</PrimaryButtons>
-                </View>
-            </View>
+import PrimaryButton from '../components/PrimaryButton';
+
+function StartGameScreen({onPickNumber}) {
+  const [enteredNumber, setEnteredNumber] = useState('');
+
+  function numberInputHandler(enteredText) {
+    setEnteredNumber(enteredText);
+  }
+
+  function resetInputHandler() {
+    setEnteredNumber('');
+  }
+
+  function confirmInputHandler() {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
+      return;
+    }
+
+    onPickNumber(chosenNumber);
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        onChangeText={numberInputHandler}
+        value={enteredNumber}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
         </View>
-
-    );
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
 }
 
+export default StartGameScreen;
+
 const styles = StyleSheet.create({
-    inputContainer: {
-        color: "black",
-        backgroundColor: "#72063c",
-        padding: 16,
-        marginHorizontal: 16,
-        marginTop: 100,
-        borderRadius: 8,
-        elevation: 4,
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 8,
-        shadowOpacity: 0.5,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-
-    buttonsContainer: {
-        flexDirection: "row",
-
-    },
-
-    buttonContainer: {
-        flex: 1,
-    },
-
-    numberInput: {
-        height: 50,
-        fontSize: 32,
-        borderBottomColor: "#ddb52f",
-        borderBottomWidth: 2,
-        width: 50,
-        marginVertical: 8,
-        textAlign: "center",
-        fontWeight: "bold",
-        color: "#ddb52f",
-        padding: 8,
-    }
-})
+  inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#3b021f',
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
