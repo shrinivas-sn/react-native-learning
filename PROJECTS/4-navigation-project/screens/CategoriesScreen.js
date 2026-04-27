f80240a3a1f6f48ecff2e943aec855f11fc8d239
shrinivas-sn <shrinusn2001@gmail.com>
2026-04-27 11:59:31 +0530

    Installed React native Navigation packages for navigation property

--- PROJECTS/4-navigation-project/screens/CategoriesScreen.js
+++ PROJECTS/4-navigation-project/screens/CategoriesScreen.js
@@ -1,16 +1,31 @@
-import { Text, View } from "react-native";
-import { FlatList } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
-import CategoryGridTile from "../components/CategoryGridTile";
-export default function CategoriesScreen() {
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate('MealsOverview');
+    }
+
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
   return (
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-        <CategoryGridTile title={item.title} color={item.color} />
-      )}
+      renderItem={renderCategoryItem}
       numColumns={2}
     />
   );
 }
+
+export default CategoriesScreen;
